18405ab547cb4b2435e2964eae7cd7d71e0896b3
Rushi007143 <rushikeshthul007@gmail.com>
2025-07-19 01:45:21 +0530

    commit changes

---
+++ src/main/java/com/example/makemytrip/hotel.java
@@ -0,0 +1,13 @@
+package com.example.makemytrip;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class hotel {
+
+    @GetMapping("/hotel")
+    public String getData () { return "Please book your hotel ticket in bengaluru 60% discount";
+
+    }
+}
